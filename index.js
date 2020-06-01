174832df2e23867ff98f922b5b1c37f51b76a5a7
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-01 14:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeli = [];
 
 var takeANumber = (line, name) => {
   line.push(name)
-  return `Welcome, ${name}.  You are number ${line.length} in line.`;
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 };
 
 var nowServing = (line) => {
